dbfd03ab6cbd6c4e2a6219037abdd7d241c51033
Yusof <117531058+yzffkindacrzy@users.noreply.github.com>
2023-01-11 13:07:56 +0000

    added zookeeper upgrade + made the screen wider.

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,69 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
-  let clicks = 500;
+  let bananas = 0;
 
   onMount(() => {
     document.getElementById("monkey").onclick = () => {
-      clicks += 1
-    }
+      bananas += 1;
+    };
 
     document.getElementById("buy-autoclicker").onclick = () => {
-      if (!(clicks >= 10)) return
-      clicks -= 10
+      if (!(bananas >= 10)) return;
+      bananas -= 10;
 
       setInterval(() => {
-        clicks += 1
-      }, 3000)
-    }
+        bananas += 1;
+      }, 3000);
+    };
 
     document.getElementById("buy-farmer").onclick = () => {
-      if (!(clicks >= 50)) return
-      clicks -= 50
+      if (!(bananas >= 50)) return;
+      bananas -= 50;
 
       setInterval(() => {
-        clicks += 5000
-      })
-    }
+        bananas += 5;
+      }, 1000);
+    };
 
+    document.getElementById("buy-zookeeper").onclick = () => {
+      if (!(bananas >= 250)) return;
+      bananas -= 250;
 
-   })
+      setInterval(() => {
+        bananas += 10;
+      }, 500);
+    };
+  });
 </script>
 
 <main>
-  <div class="container"><h1>Monkey Clicker</h1>
-    <h2>You have {clicks} bananas</h2>
-    <img alt="monkey" id="monkey" src="https://media.npr.org/assets/img/2017/09/12/macaca_nigra_self-portrait-3e0070aa19a7fe36e802253048411a38f14a79f8-s1100-c50.jpg" width="50" height="50"/>
+  <div class="container">
+    <h1>Monkey Clicker</h1>
+    <h2>You have {bananas} bananas</h2>
+    <img
+      alt="monkey"
+      id="monkey"
+      src="https://media.npr.org/assets/img/2017/09/12/macaca_nigra_self-portrait-3e0070aa19a7fe36e802253048411a38f14a79f8-s1100-c50.jpg"
+      width="50"
+      height="50"
+    />
     <div id="upgrades">
       <h2>Upgrades</h2>
       <div id="autoclicker" class="upgrade">
-        <h3>Autoclicker</h3> 
+        <h3>Autoclicker</h3>
         <h3>Cost 10 bananas</h3>
         <button id="buy-autoclicker">Buy</button>
       </div>
       <div id="farmers" class="upgrade">
-        <h3>Farmers</h3> 
+        <h3>Farmers</h3>
         <h3>Cost 50 bananas</h3>
         <button id="buy-farmer">Buy</button>
+      </div>
+      <div id="zookeepers" class="upgrade">
+        <h3>Zookeeper</h3>
+        <h3>Cost 250 bananas</h3>
+        <button id="buy-zookeeper">Buy</button>
       </div>
     </div>
   </div>
@@ -52,7 +71,7 @@
 
 <style>
   :root {
-    background-color: rgb(249, 97, 97);
+    background-color: rgb(255, 255, 255);
   }
 
   button {
@@ -67,15 +86,11 @@
   }
 
   .container {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
     background-color: gray;
-    padding: 50px;
+    padding: 30px;
     text-align: center;
     border: 10px solid;
-    transition: 350ms
+    transition: 350ms;
   }
 
   .container:hover {
@@ -84,10 +99,10 @@
 
   #upgrades {
     background-color: gray;
-    padding: 20px;
+    padding: 10px;
     text-align: center;
     border: 10px solid;
-    transition: 350ms
+    transition: 350ms;
   }
 
   #upgrades:hover {
@@ -95,13 +110,13 @@
   }
 
   .upgrade {
-    padding: 10px;
+    padding: 5px;
     background-color: purple;
     border: 5px solid;
   }
 
   #monkey {
-    transition: 0.3s
+    transition: 0.3s;
   }
 
   #monkey:hover {
@@ -110,7 +125,7 @@
   }
 
   #monkey:active {
-    width:50px;
-    height:50px;
+    width: 50px;
+    height: 50px;
   }
 </style>
